72c36c1b2789a50aae2488c747b1e1c4e5381c3e
Tran Thien <thien.tran@saigonvalley.com>
2022-11-23 00:25:12 +0700

    first commit

---
+++ lua/user/colorscheme.lua
@@ -0,0 +1,13 @@
+-- local colorscheme = "tokyonight"
+
+-- local status_ok, _ = pcall(vim.cmd, "colorscheme " .. colorscheme)
+-- if not status_ok then
+--   return
+-- end
+
+
+-- Lua
+require('onedark').setup {
+  style = 'darker'
+}
+require('onedark').load()
