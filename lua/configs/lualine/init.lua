ea16b3a7b6bcfc6c578385563e9fd6a8bc893604
Tran Thien <thien.tran@saigonvalley.com>
2023-03-15 11:13:53 +0700

    lualine

--- lua/configs/lualine/init.lua
+++ lua/configs/lualine/init.lua
@@ -72,9 +72,9 @@ lualine.setup({
 		always_divide_middle = true,
 	},
 	sections = {
-		lualine_a = { branch, diagnostics },
-		lualine_b = { mode },
-		lualine_c = {},
+		lualine_a = { mode },
+		lualine_b = { branch, diagnostics },
+		lualine_c = { },
 		-- lualine_x = { "encoding", "fileformat", "filetype" },
 		lualine_x = { diff, spaces, "encoding", filetype },
 		lualine_y = { location },
